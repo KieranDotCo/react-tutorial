891a37aefb5121bc2b4a2f16c415c2ac814e6cc5
Kieran Whiteman <kieran@kieran.co>
2019-11-21 13:16:42 +0000

    Updates

---
+++ precache-manifest.7f880614c9bd51ff4ce7d3aff1b7eb4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eb40d6edebed76990eef5255b7fbf6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "5764e23dd013ab1c83a6",
+    "url": "/react-tutorial/static/css/main.c4f668f1.chunk.css"
+  },
+  {
+    "revision": "c59f8990ac29f167a0c1",
+    "url": "/react-tutorial/static/js/2.36c9c7e5.chunk.js"
+  },
+  {
+    "revision": "5764e23dd013ab1c83a6",
+    "url": "/react-tutorial/static/js/main.ad91476f.chunk.js"
+  },
+  {
+    "revision": "206bcb477d16781e2606",
+    "url": "/react-tutorial/static/js/runtime-main.dc760d74.js"
+  }
+]);
